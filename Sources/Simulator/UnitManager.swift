bd0b3128c96b9fc6764d322de8e738d4fe563f6a
BugenZhao <i@bugenzhao.com>
2020-04-22 16:34:56 +0800

    Added UnitManager

--- Sources/Simulator/UnitManager.swift
+++ Sources/Simulator/UnitManager.swift
@@ -7,4 +7,37 @@
 
 import Foundation
 
-class UnitManager { }
+public class UnitManager {
+    let wireManager = WireManager()
+    var units: [UnitName: Unit] = [:]
+
+    public func addBasicUnit(
+        unitName: UnitName,
+        inputWires: [WireName],
+        outputWires: [WireName],
+        logic: @escaping (WireManager) -> Void) {
+        guard !units.keys.contains(unitName) else {
+            fatalError(SimulatorError.UnitManagerDuplicateName.rawValue)
+        }
+        let unit: Unit = BasicUnit(unitName, inputWires, outputWires, logic)
+        inputWires.forEach { wireName in wireManager[wireName].to.append(unitName) }
+        outputWires.forEach { wireName in wireManager[wireName].from = unitName }
+        units[unitName] = unit
+    }
+
+    func stablize() {
+        wireManager.clearCheckpoint()
+        repeat {
+            units.values.forEach { $0.logic(wireManager) }
+        } while(wireManager.doCheckpoint() == false)
+    }
+
+    func rise() {
+        units.values.forEach { $0.onRising(wireManager) }
+    }
+
+    public func clock() {
+        rise()
+        stablize()
+    }
+}
