fe2d2f6a9a46a22d6fbd11535705006ae914be9a
BugenZhao <i@bugenzhao.com>
2020-04-22 16:30:38 +0800

    Added wireManager.checkpoint

--- Sources/Simulator/WireManager.swift
+++ Sources/Simulator/WireManager.swift
@@ -10,13 +10,31 @@ import Foundation
 @dynamicMemberLookup
 public class WireManager {
     var wires: [WireName: Wire] = [:]
+    var checkpoint: [WireName: UInt64] = [:]
 
     subscript(dynamicMember wireName: WireName) -> Wire {
         get {
             if let wire = wires[wireName] { return wire }
-            let wire = Wire(name: wireName, value: 0)
+            let wire = Wire(wireName: wireName, value: 0)
             wires[wireName] = wire
             return wire
         }
+    }
+
+    subscript(_ wireName: WireName) -> Wire {
+        get {
+            return self[dynamicMember: wireName]
+        }
+    }
+
+    public func clearCheckpoint() {
+        checkpoint.removeAll()
+    }
+
+    public func doCheckpoint() -> Bool {
+        var newCheckpoint: [WireName: UInt64] = [:]
+        defer { checkpoint = newCheckpoint }
+        wires.forEach { wireName, wire in newCheckpoint[wireName] = wire.value }
+        return checkpoint == newCheckpoint
     }
 }
