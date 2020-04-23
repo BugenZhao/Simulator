e4d01ed2a060e390c7fea3aa313b8b09342dd3ca
BugenZhao <i@bugenzhao.com>
2020-04-24 00:47:40 +0800

    Added: more extension of Numeric

--- Sources/SimulatorLib/Utilities/NumericExtension.swift
+++ Sources/SimulatorLib/Utilities/NumericExtension.swift
@@ -9,8 +9,14 @@ import Foundation
 
 public extension Bool {
     var u64: UInt64 { return self ? 1 : 0 }
+    var u32: UInt32 { return self ? 1 : 0 }
+    var u16: UInt16 { return self ? 1 : 0 }
+    var u8: UInt8 { return self ? 1 : 0 }
 }
 
 public extension BinaryInteger {
     var u64: UInt64 { return UInt64(self) }
+    var u32: UInt32 { return UInt32(self) }
+    var u16: UInt16 { return UInt16(self) }
+    var u8: UInt8 { return UInt8(self) }
 }
