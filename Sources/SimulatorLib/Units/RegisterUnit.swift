19f8d35401e72cf7d0b399dbc33ed6cc4e0038b2
BugenZhao <i@bugenzhao.com>
2020-04-23 22:37:42 +0800

    Added: RegisterUnit

---
+++ Sources/SimulatorLib/Units/RegisterUnit.swift
@@ -0,0 +1,44 @@
+//
+//  RegisterUnit.swift
+//  SimulatorLib
+//
+//  Created by Bugen Zhao on 2020/4/23.
+//
+
+import Foundation
+
+class RegisterUnit: Unit, Addressable {
+    var name: UnitName
+    var inputWires: [WireName]
+    var outputWires: [WireName]
+    var logic: (WireManager) -> Void = { _ in return }
+    var onRising: (WireManager) -> Void = { _ in return }
+
+    var data: Data
+
+    var realLogic: (WireManager, RegisterUnit) -> Void
+    var realOnRising: (WireManager, RegisterUnit) -> Void
+
+    init(_ unitName: UnitName,
+        _ inputWires: [WireName],
+        _ outputWires: [WireName],
+        _ logic: @escaping (WireManager, RegisterUnit) -> Void,
+        _ onRising: @escaping (WireManager, RegisterUnit) -> Void,
+        _ registerCount: Int) {
+        self.name = unitName
+        self.inputWires = inputWires
+        self.outputWires = outputWires
+        
+        self.data = Data(count: registerCount)
+
+        self.realLogic = logic
+        self.realOnRising = onRising
+
+        self.logic = { wm in self.realLogic(wm, self) }
+        self.onRising = { wm in self.realOnRising(wm, self) }
+    }
+
+    func copied() -> Self {
+        return RegisterUnit(name, inputWires, outputWires, realLogic, realOnRising, data.count) as! Self
+    }
+}
