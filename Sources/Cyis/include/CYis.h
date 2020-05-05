79b32c2e60f44ae3475e3223e2d5346027cc84fb
BugenZhao <i@bugenzhao.com>
2020-05-05 12:03:19 +0800

    Cleanup

--- Sources/Cyis/include/CYis.h
+++
@@ -1,9 +0,0 @@
-#ifndef SIMULATOR_CYIS_H
-#define SIMULATOR_CYIS_H
-
-#include "isa.h"
-#include <stdlib.h>
-
-int runYis(const char *yo_path, stat_t *ep, state_rec *sp);
-
-#endif  // SIMULATOR_CYIS_H
